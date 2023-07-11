1aea93883732e8a1df517e616f9f17ed92201706
jaidesai <jay.d@peerbits.com>
2023-07-11 21:53:26 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,34 +1,59 @@
-import React from "react"
-import ReactDOM  from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const treeOfParent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", { id: "heading" }, "Hello World From React!")
-  )
-);
+// const heading = React.createElement("h1", { id: "heading" }, "Hello Jay");
 
-const siblingStructure = React.createElement(
-  "div",
-  { id: "parent" },[
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading1" }, "Namaste React 🚀!"),
-    React.createElement("h1", { id: "heading2" }, "By Jay Desai")
-  ])
-]
-);
+// jsx is javascript syntax for creating react element
+// jsx is like a convention when we kind merge for html and js together
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World From React!"
-);
+// jsx - is not html in JS
+// JSX - HTML - like syntax or xml-like syntax
+//JSX (transpiled before it reaches the JS) - PARCEL - Babel
 
-// console.log(siblingStructure);
+// JSX = React.createElement => ReactElement - JSObject => HTMLElement(render)
+
+const TitleComponent = () => {
+  return (
+    <h1 className="head" tabIndex="5">
+      Namaste React Using JSX
+    </h1>
+  );
+};
+
+//Functional Component
+//1.  a function return some peace of jsx code it's functional component
+//2. a function which return react element
+
+const number = 1000;
+
+const HeadingComponent1 = () => {
+  return (
+    <div className="container">
+      {TitleComponent()}
+      <TitleComponent />
+      <TitleComponent></TitleComponent>
+      <h1 className="heading">Namaste React Functional Component🚀</h1>
+    </div>
+  );
+};
+
+//React-Fragment - behave like an empty tag
+// JSX don't allowed multiple root element in we can have only one root element and inside that whatever
+
+const HeadingComponent = () => {
+  return (
+    // <React.Fragment>
+    <>
+      <div className="container">
+        <TitleComponent />
+        <h1 className="heading">Namaste React Functional Component🚀</h1>
+      </div>
+      <div id="conteriner2"></div>
+    </>
+    // </React.Fragment>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(siblingStructure);
+root.render(<HeadingComponent />);
